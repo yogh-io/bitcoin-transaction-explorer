28cd3aea8ac927c662b07cc6683b833e5c0e1f2d
Jorn Cruijsen <jorncruijsen@gmail.com>
2014-11-28 01:27:05 +0100

    Timeout for external API

--- bitcoin-transactions-server/src/main/java/com/yoghurt/crypto/transactions/server/servlets/util/HttpClientProxy.java
+++ bitcoin-transactions-server/src/main/java/com/yoghurt/crypto/transactions/server/servlets/util/HttpClientProxy.java
@@ -2,7 +2,6 @@ package com.yoghurt.crypto.transactions.server.servlets.util;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.http.HttpException;
@@ -11,23 +10,29 @@ import org.apache.http.HttpStatus;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 /**
  * Wrapper to get remote content and return it as String.
  */
 public class HttpClientProxy {
+  private final static RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(5 * 1000).build();
 
   public static InputStream getRemoteContent(final HttpClient client, final String url) throws URISyntaxException, ClientProtocolException, IOException, ParseException, HttpException {
-    final URI uri = new URI(url);
-    final HttpGet getRequest = new HttpGet(uri);
-    final HttpResponse httpResponse = client.execute(getRequest);
+    final HttpResponse httpResponse = client.execute(new HttpGet(url));
 
     if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
       return httpResponse.getEntity().getContent();
     } else {
       throw new HttpException(httpResponse.getStatusLine().toString() + EntityUtils.toString(httpResponse.getEntity()));
     }
+  }
+
+  public static CloseableHttpClient buildProxyClient() {
+    return HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
   }
 }
