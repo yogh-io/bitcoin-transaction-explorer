31af92be80c932b721612d561e80f64383fdfde7
Jorn Cruijsen <jorncruijsen@gmail.com>
2015-07-06 13:34:44 +0200

    Fixed bug where pointer was incremented when ...
    ... it shouldn't

--- bitcoin-transactions-core/src/main/java/com/yoghurt/crypto/transactions/client/util/transaction/ScriptEncodeUtil.java
+++ bitcoin-transactions-core/src/main/java/com/yoghurt/crypto/transactions/client/util/transaction/ScriptEncodeUtil.java
@@ -40,7 +40,8 @@ public final class ScriptEncodeUtil {
   private static int encodePushDataOperation(final int opcode, final byte[] bytes, final byte[] partBytes, int pointer) {
     switch (opcode) {
     case 76:
-      bytes[pointer = pointer + 1] = (byte) partBytes.length;
+      bytes[pointer] = (byte) partBytes.length;
+      pointer++;
     default:
       System.arraycopy(partBytes, 0, bytes, pointer, partBytes.length);
       return pointer + partBytes.length;
